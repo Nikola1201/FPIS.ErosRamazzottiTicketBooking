ac1ff93babb3ac31d36e5d3fb3cf18632efdce0b
Nikola1201 <160769567+Nikola1201@users.noreply.github.com>
2026-05-26 23:13:28 +0200

    feat(infrastructure): Dodao repozitorijume

---
+++ Infrastructure/Repositories/IRepository.cs
@@ -0,0 +1,79 @@
+﻿using FPIS.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace FPIS.Infrastructure.Repositories;
+
+public interface IRepository<TEntity> where TEntity : class
+{
+    Task<IEnumerable<TEntity>> GetAllAsync(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        bool asNoTracking = false,
+        params Expression<Func<TEntity, object>>[] includes);
+
+
+    Task<TEntity?> GetByIdAsync(Guid id);
+    Task AddAsync(TEntity entity);
+    void Update(TEntity entity);
+    void Delete(TEntity entity);
+    void RemoveRange(ICollection<TEntity> entities);
+    Task<int> CountAsync(Expression<Func<TEntity, bool>>? predicate = null);
+}
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+{
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public Repository(ApplicationDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync(
+        Expression<Func<TEntity, bool>>? predicate = null,
+        bool asNoTracking = false,
+        params Expression<Func<TEntity, object>>[] includes)
+    {
+        IQueryable<TEntity> query = _dbSet;
+
+        if (asNoTracking)
+            query = query.AsNoTracking();
+        else
+        {
+            query = query.AsTracking();
+        }
+        foreach (var include in includes)
+            query = query.Include(include);
+
+        if (predicate != null)
+            query = query.Where(predicate);
+
+        return await query.ToListAsync();
+    }
+
+    public async Task<TEntity?> GetByIdAsync(Guid id)
+        => await _dbSet.FirstOrDefaultAsync(e => EF.Property<Guid>(e, "Id") == id);
+
+    public async Task AddAsync(TEntity entity)
+        => await _dbSet.AddAsync(entity);
+
+    public void Update(TEntity entity)
+        => _dbSet.Update(entity);
+
+    public void Delete(TEntity entity)
+        => _dbSet.Remove(entity);
+
+    public void RemoveRange(ICollection<TEntity> entities)
+    {
+        _dbSet.RemoveRange(entities);
+    }
+
+    public Task<int> CountAsync(Expression<Func<TEntity, bool>>? predicate = null)
+    {
+        if (predicate != null)
+            return _dbSet.CountAsync(predicate);
+        return _dbSet.CountAsync();
+
+    }
+}
---
+++ Infrastructure/Repositories/IUnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Collections.Concurrent;
+
+namespace FPIS.Infrastructure.Repositories;
+public interface IUnitOfWork : IDisposable
+{
+    Task<IDbContextTransaction> BeginTransactionAsync();
+    IRepository<TEntity> Repository<TEntity>() where TEntity : class;
+    Task<int> SaveChangesAsync();
+}
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    private readonly ConcurrentDictionary<Type, object> _repositories = new();
+
+    public UnitOfWork(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IRepository<TEntity> Repository<TEntity>() where TEntity : class
+    {
+        var type = typeof(TEntity);
+        if (!_repositories.TryGetValue(type, out var repository))
+        {
+            repository = new Repository<TEntity>(_context);
+            _repositories[type] = repository;
+        }
+        return (IRepository<TEntity>)repository;
+
+    }
+
+    public async Task<int> SaveChangesAsync()
+        => await _context.SaveChangesAsync();
+    public Task<IDbContextTransaction> BeginTransactionAsync() => _context.Database.BeginTransactionAsync();
+
+    public void Dispose()
+        => _context.Dispose();
+
+}
